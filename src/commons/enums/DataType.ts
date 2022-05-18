2f609e15d88c3f13239d953d60987f5ce2f4b02d
caioreigot <caiocrrodrigues2004@gmail.com>
2022-05-18 00:46:06 -0300

    Added new values

--- src/commons/enums/DataType.ts
+++ src/commons/enums/DataType.ts
@@ -1,9 +1,9 @@
 /* Obs: Os valores são iguais aos nomes 
 para facilitar o Debug */
 enum DataType {
+  KNOWN_HOSTS = 'KNOWN_HOSTS',
   PEER_INTRODUCTION = 'PEER_INTRODUCTION',
   NAME_CHANGED = 'NAME_CHANGED',
-  KNOWN_HOSTS = 'KNOWN_HOSTS',
   MESSAGE = 'MESSAGE',
   STATE = 'STATE'
 }
